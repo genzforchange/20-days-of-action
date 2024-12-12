76e60f62506bbf6732b6991fc905f84150978795
sofia <sofiaongele@gmail.com>
2024-12-12 12:21:34 -0800

    Initial commit

---
+++ actions.js
@@ -0,0 +1,124 @@
+const ACTIONS = [
+    {
+        date: "12/12/24",
+        title: "Stay in the loop!",
+        context: "follow us, donate, and share with others",
+        links: {
+            "follow us!": "link",
+            "donate": "link",
+            "share with friends!": "link",
+        }
+    },
+    {
+        date: "12/13/24",
+        title: "Call your state government and urge them to flex highway funding for public transit",
+        context: "follow us, donate, and share with others",
+        links: {
+            "follow us!": "link",
+        }
+    },
+    {
+        date: "12/14/24",
+        title: "Donate to Act Up NY",
+        context: "follow us, donate, and share with others",
+        links: {
+            "Donate": "https://give.actupny.com/donate/",
+        }
+    },
+    {
+        date: "12/15/24",
+        title: "Tell Biden to Commute Sentences on Death Row",
+        context: "follow us, donate, and share with others",
+        links: {
+            "Sign On": "https://actionnetwork.org/petitions/biden-demolish-the-death-chamber",
+        }
+    },
+    {
+        date: "12/16/24",
+        title: "Join YDSA workplace organizing event",
+        context: "Starting a union drive on your campus? Interested in learning the basics of labor organizing? Join the Youth Labor Committee in our labor organizing convo training Monday, December 16th at 5pm pacific/8pm eastern!",
+        links: {
+            "Sign Up": "https://actionnetwork.org/events/ylc-labor-101-organizing-convo-training?source=shortlink",
+        }
+    },
+    {
+        date: "12/17/24",
+        title: "Donate to Point of Pride/donate clothes to LGBTQ center",
+        context: "follow us, donate, and share with others",
+        links: {
+            "Donate To Point of Pride": "https://www.pointofpride.org/donate",
+            "Donate Clothes": "link"
+        }
+    },
+    {
+        date: "12/18/24",
+        title: "Sign up to volunteer @ / Follow social media of your local immigrant rights rapid response organization",
+        context: "follow us, donate, and share with others",
+        links: {
+            "follow socials": "link",
+            "sign up to volunteer": "link"
+        }
+    },
+    {
+        date: "12/19/24",
+        title: "Volunteer at / donate to a food drive (note that money is more effective than cans)",
+        context: "follow us, donate, and share with others",
+        links: {
+            "donate": "link",
+            "sign up to volunteer": "link"
+        }
+    },
+    {
+        date: "12/20/24",
+        title: "Get tapped in with local mutual aid group",
+        context: "follow us, donate, and share with others",
+        links: {
+            "donate": "link",
+            "sign up to volunteer": "link"
+        }
+    },
+    {
+        date: "12/21/24",
+        title: "Dinner table talking points",
+        context: "start talking to the people around you about the issues that matter!",
+        links: {
+            "messaging guide": "https://docs.google.com/document/d/1wIcZE31O7QB1ot8bnBpmlvSxLTM0-HN3RIMq-N9k4PI/edit?usp=sharing",
+        }
+    },
+    {
+        date: "12/22/24",
+        title: "Support Zohran for Mayor",
+        context: "start talking to the people around you about the issues that matter!",
+        links: {
+            "donate": "link",
+            "volunteer": "link"
+        }
+    },
+    {
+        date: "12/23/24",
+        title: "Schools out, read a book!",
+        context: "start talking to the people around you about the issues that matter!",
+        links: {
+            "book": "link",
+        }
+    },
+    {
+        date: "12/24/24",
+        title: "Donate to / follow UFW",
+        context: "start talking to the people around you about the issues that matter!",
+        links: {
+            "donate": "link",
+            "follow": "link"
+        }
+    },
+    {
+        date: "12/25/24",
+        title: "Domestic Violence victim funds (legal aid, pro bono lawyer, clothes, document gathering, housing)",
+        context: "start talking to the people around you about the issues that matter!",
+        links: {
+            "donate": "link",
+            "follow": "link"
+        }
+    },
+    
+]
